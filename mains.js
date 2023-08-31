2d9bcce1195074c854954955a143c62317edc955
Erick <erick_mora@hotmail.es>
2023-08-30 20:18:13 -0500

    Terminando funcionalidad de js

--- mains.js
+++ mains.js
@@ -12,8 +12,8 @@ async function getData(param){
     }
 }
 async function searchPokemon(){
-    const valueOption = document.getElementById('searchOption').value;
-    const valueInput = valueOption == 'id' ? document.getElementById('idInput').value : document.getElementById('nameInput').value;
+    let valueInput = validateInput();
+
     const data = await getData(valueInput);
 
     document.getElementById('namePokemon').textContent = data.name;
@@ -21,10 +21,81 @@ async function searchPokemon(){
 
     
 }
-
-function showDetails(){
-    const namePokemon = document.getElementById('namePokemon');
-    const imagePokemon = document.getElementById('imagePokemon');
-    const moreInormation = document.getElementById('moreInformation');
+function validateInput(){
+    const valueOption = document.getElementById('searchOption').value;
+    const valueInput = valueOption == 'id' ? document.getElementById('idInput').value : document.getElementById('nameInput').value;
+    return valueInput;
 }
-console.log(getData(25));
+function showDetails(){
+    let valueInput = validateInput();
+    const inputValue = document.getElementById('idInput').value
+
+    getData(valueInput).then(pokemonData => {
+        const modal = document.getElementById('modal');
+        modal.innerHTML= '';
+
+        const pokemonName = document.createElement('h2');
+            pokemonName.textContent = pokemonData.name;
+
+            const typesHeading = document.createElement('h3');
+            typesHeading.textContent = 'Tipos:';
+            const typesList = document.createElement('ul');
+            const types = pokemonData.types.map(typeObj => {
+                const typeItem = document.createElement('li');
+                typeItem.textContent = typeObj.type.name;
+                return typeItem;
+            });
+            types.forEach(type => typesList.appendChild(type));
+
+            const abilitiesHeading = document.createElement('h3');
+            abilitiesHeading.textContent = 'Habilidades:';
+            const abilitiesList = document.createElement('ul');
+            const abilities = pokemonData.abilities.map(abilityObj => {
+                const abilityItem = document.createElement('li');
+                abilityItem.textContent = abilityObj.ability.name;
+                return abilityItem;
+            });
+            abilities.forEach(ability => abilitiesList.appendChild(ability));
+
+            const statsHeading = document.createElement('h3');
+            statsHeading.textContent = 'Estadísticas:';
+            const statsList = document.createElement('ul');
+            const stats = pokemonData.stats.map(statObj => {
+                const statItem = document.createElement('li');
+                statItem.textContent = `${statObj.stat.name}: ${statObj.base_stat}`;
+                return statItem;
+            });
+            stats.forEach(stat => statsList.appendChild(stat));
+            // Agregar elementos al modal
+            modal.appendChild(pokemonName);
+            modal.appendChild(typesHeading);
+            modal.appendChild(typesList);
+            modal.appendChild(abilitiesHeading);
+            modal.appendChild(abilitiesList);
+            modal.appendChild(statsHeading);
+            modal.appendChild(statsList);
+    })
+}
+function addToTable() {
+    const pokemonName = document.getElementById('namePokemon').textContent;
+    const pokemonID = document.getElementById('idInput').value;
+
+    // Verificar si el Pokémon ya existe en la tabla
+    const tableRows = document.querySelectorAll('#pokemonTable tbody tr');
+    for (const row of tableRows) {
+        const existingPokemonName = row.querySelector('td:first-child').textContent;
+        if (existingPokemonName === pokemonName) {
+            alert('¡Este Pokémon ya está en la tabla!');
+            return;
+        }
+    }
+
+    // Si no existe, agregar el Pokémon a la tabla
+    if (tableRows.length < 6) {
+        const newRow = `<tr><td>${pokemonName}</td><td>${pokemonID}</td></tr>`;
+        const tableBody = document.querySelector('#pokemonTable tbody');
+        tableBody.innerHTML += newRow;
+    } else {
+        alert('¡La tabla ya tiene el máximo de 6 Pokémon!');
+    }
+}
